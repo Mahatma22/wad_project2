465f74bebbd0da9823bc7ae49bfe94b95cffb915
Mahatma Rusyda Afif <90493429+Mahatma22@users.noreply.github.com>
2023-10-20 18:19:40 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Latihan 2</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body bgcolor="whitesmoke">
+<br>
+<h1 style="font-family: sans-serif; ">Tugas 2</h1>
+<form>
+	<div class="mb-3">
+	  <label for="formGroupExampleInput" class="form-label">Input Number</label>
+	  <input type="number" class="form-control" id="formGroupExampleInput" placeholder="Input number">
+	</div>
+	<div class="col-auto">
+		<label for="OptionInput" class="form-label">Option</label>
+	    <select class="form-select" id="autoSizingSelect">
+	      <option selected>Choose...</option>
+	      <option value="1">One</option>
+	      <option value="2">Two</option>
+	      <option value="3">Three</option>
+	    </select>
+	</div>
+	<table border="0" cellspacing="0" cellpadding="8" align="center">
+		<tr>
+			<td colspan="2" align="center">
+				<button type="reset">Calculate</button>
+			</td>
+		</tr>
+	</table>
+</form>
+
+</body>
+</html>
